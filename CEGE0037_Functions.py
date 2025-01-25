15740b49c5f75e368792dd0f789079765a5919d4
Ali Talha Atici <52800054+alitatici@users.noreply.github.com>
2025-01-25 15:03:26 +0000

    Manually add files to create Streamlit App.

---
+++ CEGE0037_Functions.py
@@ -0,0 +1,145 @@
+import numpy as np
+import pandas as pd
+import scipy.io
+import scipy.spatial
+from scipy.interpolate import interp1d
+
+def vulnerability_funtions(vulnerability_models_list):
+    """
+    This is the definition of the vulnerability_funtions function, which takes a single argument
+    vulnerability_models - the name of an Excel file that contains the data to be processed.
+    """
+    # read the excel file and extract data from the first sheet
+    df = pd.read_excel(vulnerability_models_list, sheet_name=1)
+
+    # initialize an empty dictionary to store the results
+    vulnerability_dict = {}
+
+    # loop through each row in the dataframe
+    for index, row in df.iterrows():
+        # get the value of the "vulnStrFL" column for the current row
+        vulnStrFL = row['vulnStrFL']
+
+        # get a list of tuples, each tuple containing the value of the "LR(hw=x)" column
+        # (where x is a number) and the corresponding "hw" value
+        lr_hw_pairs = [(hw, row['LR(hw={})'.format(hw)]) for hw in [0, 0.5, 1, 1.5, 2, 3, 4, 5, 6]]
+
+        # add the current vulnStrFL value as the key in the "result" dictionary,
+        # and the corresponding lr_hw_pairs list as the value
+        vulnerability_dict[vulnStrFL] = lr_hw_pairs
+    return vulnerability_dict
+
+
+def interpolation(d, x):
+    """
+    This function performs linear interpolation on two points `d` and a value `x`.
+    Arguments:
+        d: (List[Tuple[float, float]]) - a list of two tuples, each tuple containing an `x` value and a corresponding `y` value
+        x: (float) - the value at which the interpolation should be performed
+    Returns:
+        output_int: float - the linearly interpolated value at `x`
+    """
+    # Perform linear interpolation calculation
+    x_val = np.array([t[0] for t in d])
+    y_val = np.array([t[1] for t in d])
+    # output_int = d[0][1] + (x - d[0][0]) * ((d[1][1] - d[0][1])/(d[1][0] - d[0][0]))
+    f = interp1d(x_val, y_val)
+    output_int = float(f(x))
+
+    # Return the interpolated value
+    return output_int
+
+def physical_impact_buildings(Buildings, flood_damage_dict):
+    """
+    This function calculates the physical impact of a flood on buildings.
+    `Buildings` is a Pandas DataFrame that contains the data of the buildings.
+    `flood_damage_dict` is a dictionary that maps vulnerability strings to damage functions.
+    The function returns a list of damage levels for each building.
+    """
+    # Get the vulnerability strings and water heights from the buildings data
+    vulnerability_strings = Buildings['vulnStrFL']
+    water_height = Buildings['water_height']  # Need to estimate this one and add it to "Buildings"
+
+    # Initialize an empty list to store the damage levels
+    dmg_level = []
+
+    # Loop through each building
+    for i in range(len(vulnerability_strings)):
+        # Get the vulnerability string and the corresponding damage function
+        vuln_string = vulnerability_strings[i]
+        try:
+            vuln_function = flood_damage_dict[vuln_string]
+        except:
+            vuln_string = vuln_string.replace('\n', '')
+            vuln_function = flood_damage_dict[vuln_string]
+
+        # Calculate the damage level using the interpolation function
+        damage = interpolation(vuln_function, water_height[i])
+
+        # Append the damage level to the list
+        dmg_level.append(damage)
+
+    # Return the list of damage levels
+    return dmg_level
+
+
+# GOAL 2
+def water_permeation_and_recovery_days(buildings_df, permeation_rate, loss_ratio_limit, days_for_recovery_vulnerability):
+    """
+        Water permeation time and recovery time are calculated in weeks.
+    """
+    recovery_time_array = []
+    water_permeation_time_array = []
+    # days_for_recovery_vulnerability = restoration_times()
+
+    for i, row in buildings_df.iterrows():
+        water_that_should_permeate = row['water_height'] - 0.25
+        if water_that_should_permeate > 0:
+            water_permeation_time = water_that_should_permeate / permeation_rate
+        else:
+            water_permeation_time = 0
+
+        recovery_time = restoration_time_calculation(row, days_for_recovery_vulnerability, loss_ratio_limit)
+
+        recovery_time_array.append(recovery_time)
+        water_permeation_time_array.append(water_permeation_time)
+
+    buildings_df['water_permeation_time_in_days'] = water_permeation_time_array
+    buildings_df['recovery_time_in_days'] = recovery_time_array
+
+    return buildings_df
+
+def restoration_time_calculation(row, days_for_recovery_vulnerability, loss_ratio_limit):
+
+    vulnerability_strings = days_for_recovery_vulnerability['vulnStrFL'].tolist()
+
+    vulnStrFL_row = row['vulnStrFL'].strip()
+    Loss_Ratio = row['Loss_Ratio']
+    recovery_time = 0
+
+    for index, vulnerability_string in enumerate(vulnerability_strings):
+        if vulnerability_string == vulnStrFL_row:
+            if Loss_Ratio < loss_ratio_limit:
+                recovery_time = 0
+            else:
+                LR00 = days_for_recovery_vulnerability.loc[index, "Loss Ratio = 0.0"]
+                LR02 = days_for_recovery_vulnerability.loc[index, "Loss Ratio = 0.2"]
+                LR04 = days_for_recovery_vulnerability.loc[index, "Loss Ratio = 0.4"]
+                LR06 = days_for_recovery_vulnerability.loc[index, "Loss Ratio = 0.6"]
+                LR08 = days_for_recovery_vulnerability.loc[index, "Loss Ratio = 0.8"]
+                LR10 = days_for_recovery_vulnerability.loc[index, "Loss Ratio = 1.0"]
+
+                if 0.0 <= Loss_Ratio < 0.2:
+                    recovery_time = ((Loss_Ratio - 0.0) / 0.2) * (LR02 - LR00) + LR00
+                elif 0.2 <= Loss_Ratio < 0.4:
+                    recovery_time = ((Loss_Ratio - 0.2) / 0.2) * (LR04 - LR02) + LR02
+                elif 0.4 <= Loss_Ratio < 0.6:
+                    recovery_time = ((Loss_Ratio - 0.4) / 0.2) * (LR06 - LR04) + LR04
+                elif 0.6 <= Loss_Ratio < 0.8:
+                    recovery_time = ((Loss_Ratio - 0.6) / 0.2) * (LR08 - LR06) + LR06
+                elif 0.8 <= Loss_Ratio <= 1.0:
+                    recovery_time = ((Loss_Ratio - 0.8) / 0.2) * (LR10 - LR08) + LR08
+                else:
+                    raise ValueError("Loss_Ratio must be between 0 and 1.")
+
+    return np.round(recovery_time, 2)
---
+++ CEGE0037_Main_Streamlit.py
@@ -0,0 +1,220 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from CEGE0037_Functions import *
+
+# Streamlit App
+st.set_page_config(layout="wide")  # Set layout to wide
+st.title('CEGE0037: Reliability, Risk and Resilience Engineering 24/25')
+st.title('Group Project Tools')
+st.write("---")
+st.title("Tool 1")
+# Create two columns for layout: one for Inputs and one for Analyses
+col1, col2 = st.columns([1,1])
+
+with col1:
+    st.header("Inputs")
+    st.write("---")
+
+    # Add a file uploader to the webpage
+    st.write("You need to add a building dataset with a column named 'water_height'. The building dataset is provided in the geodatabase file, and the water height information should be extracted from the flood raster file.")
+    uploaded_file_of_buildings = st.file_uploader("Choose the Excel file of list of buildings with 'water_height'.", type=["xlsx"])
+    # uploaded_file_vulnerability_models = st.file_uploader("Choose the Excel file of vulnerability model", type=["xlsx"])
+
+    if uploaded_file_of_buildings is not None:
+        st.write("Filename of list of buildings:", uploaded_file_of_buildings.name)
+    # if uploaded_file_vulnerability_models is not None:
+    #     st.write("Filename of vulnerability model:", uploaded_file_vulnerability_models.name)
+
+    vulnerability_models_list = 'Core Materials/vulnerabilityInventory_TV0_updated.xlsx'
+
+    flood_damage_dict = vulnerability_funtions(vulnerability_models_list)
+
+    st.write("---")
+    st.write("**The vulnerability curve table and graph are provided only for visualization.**")
+
+    # Create a dropdown menu for dictionary keys
+    selected_key = st.selectbox("Select a flood vulnerability", options=list(flood_damage_dict.keys()))
+
+    # Get the selected data
+    selected_data = flood_damage_dict[selected_key]
+
+    # Convert to DataFrame for table display
+    df = pd.DataFrame(selected_data, columns=["Flood Height (m)", "Loss Ratio"])
+    table_col, plot_col= st.columns([2, 4])
+
+    with table_col:
+        st.write("Table of Data:")
+        st.dataframe(df, height=400, use_container_width=True)
+
+    with plot_col:
+        # st.write("Plot of Flood Height vs. Loss Ratio:") # Smaller size (6 inches wide, 3 inches high)
+        fig, ax = plt.subplots(figsize=(5, 3))
+        ax.plot(df["Flood Height (m)"], df["Loss Ratio"], marker="o", linestyle="-", color="blue")
+        ax.set_xlabel("Flood Height (m)", fontsize=5)
+        ax.set_ylabel("Loss Ratio", fontsize=5)
+        ax.set_title(f"Flood Height vs. Loss Ratio for {selected_key}", fontsize=6)
+        # Adjust font size for axis tick labels
+        ax.tick_params(axis='both', labelsize=5)
+        ax.set_xlim(0, 6)  # Limit x-axis between 0 and 6
+        ax.set_ylim(0, 1)  # Limit y-axis between 0 and 1
+        ax.grid(True)
+
+        # Display the plot in Streamlit
+        st.pyplot(fig)
+
+with col2:
+    st.header("Output")
+    st.write("---")
+    st.write("Loss ratio of Tomorrowville buildings following flood damage:")
+
+    # Handle the Buildings file and calculate the damage levels
+    if uploaded_file_of_buildings is not None:
+        try:
+            # Load the building data
+            Buildings = pd.read_excel(uploaded_file_of_buildings)
+
+            # Calculate the damage level using the physical_impact_buildings function
+            dmg_level = physical_impact_buildings(Buildings, flood_damage_dict)
+
+            # Add damage level to Buildings DataFrame
+            Buildings['Loss_Ratio'] = dmg_level
+
+            st.dataframe(Buildings, height=400, use_container_width=True)
+
+        except Exception as e:
+            st.error(f"Error processing buildings file: {e}")
+
+    st.write("Note: To complete the analysis for Goal 1, download the table and perform the calculations using Excel, Python, or MATLAB.")
+
+st.write("---")
+
+# Create two columns for layout: one for Inputs and one for Analyses
+col3, col4 = st.columns([1,1])
+with col3:
+
+    st.title("Tool 2")
+
+    st.header("Inputs")
+    st.write("---")
+    st.write("The requested value here represents the permeation rate of water levels in buildings.")
+    # start = st.number_input(label="Start day for resilience curve instances (days)", min_value=0, max_value=10000, value=0, step=1)
+    # end = st.number_input(label="End day for resilience curve instances (days)", min_value=0, max_value=10000, value=0, step=1)
+    # num_intervals = st.number_input(label="Number of instances", min_value=3, max_value=1000, value=3, step=1)
+    permeation_rate_mm_day = st.number_input(label="Permeation Rate (mm/day)", min_value=0, max_value=10000, value=1, step=1)
+    permeation_rate = permeation_rate_mm_day / 1000
+    st.text(f"Permeation Rate: {permeation_rate:.3f} meters/day")
+    st.write("---")
+
+    loss_ratio_limit = st.number_input(label="Loss ratio threshold that you assume for occupiability of buildings.", min_value=0.00, max_value=1.00, value=0.00, step=0.01)
+    st.write("---")
+    # intervals = np.linspace(start, end, num_intervals, endpoint=True)
+    # time_list = list(intervals)
+
+    # Define initial data as a dictionary
+    data = [
+        {"vulnStrFL": "Adb+LC+LR+1s+Res", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "Adb+LC+LR+2s+Res", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "Adb+LC+LR+3s+Res", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "Adb+LC+LR+4s+Res", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "BrX+LC+LR+1s+Res", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "BrX+LC+LR+2s+Res", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "BrX+LC+LR+3s+Res", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "BrX+LC+LR+4s+Res", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+1s+Res", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+2s+Res", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+3s+Res", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+4s+Res", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+5s+Res", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+6s+Res", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+7s+Res", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+8s+Res", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "Adb+LC+LR+1s+Com", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "Adb+LC+LR+2s+Com", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "Adb+LC+LR+3s+Com", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "Adb+LC+LR+4s+Com", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "BrX+LC+LR+1s+Com", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "BrX+LC+LR+2s+Com", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "BrX+LC+LR+3s+Com", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "BrX+LC+LR+4s+Com", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+1s+Com", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+2s+Com", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+3s+Com", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+4s+Com", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+5s+Com", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+6s+Com", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+7s+Com", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+8s+Com", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "Adb+LC+LR+1s+Ind", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "Adb+LC+LR+2s+Ind", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "Adb+LC+LR+3s+Ind", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "Adb+LC+LR+4s+Ind", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "BrX+LC+LR+1s+Ind", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "BrX+LC+LR+2s+Ind", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "BrX+LC+LR+3s+Ind", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "BrX+LC+LR+4s+Ind", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+1s+Ind", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+2s+Ind", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+3s+Ind", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+4s+Ind", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+5s+Ind", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+6s+Ind", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+7s+Ind", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "RCi+xC+LR+8s+Ind", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "StMin+LC+LR+Res", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+        {"vulnStrFL": "Agri", "Loss Ratio = 0.0": 0, "Loss Ratio = 0.2": 40, "Loss Ratio = 0.4": 80, "Loss Ratio = 0.6": 120, "Loss Ratio = 0.8": 160, "Loss Ratio = 1.0": 200},
+    ]
+
+    # Convert to DataFrame
+    df = pd.DataFrame(data)
+
+    st.write("**Restoration time for building typologies of Tomorrowville**")
+    st.write("Here, you are expected to enter the number of days required for restoration based on building types corresponding to loss ratios. You can assign days to loss ratios below the threshold you set; however, the restoration time calculation will only apply to values above the limit.")
+    st.write("Please edit the table below as needed and click 'Save & Run' to submit.")
+
+    # Display the table editor
+    days_for_recovery_vulnerability = st.data_editor(df, num_rows="dynamic", height=400, use_container_width=True)
+
+    # Function to validate if any cell contains negative values
+    def validate_no_negative_values(df):
+        for column in df.columns:
+            if df[column].dtype in ['float64', 'int64']:  # Only check numerical columns
+                if (df[column] < 0).any():
+                    st.error(f"Negative values detected in column '{column}'. Please correct them.")
+                    return False
+        return True
+
+    flag_save = 0
+    # When user submits or updates the DataFrame, validate
+    if st.button("Save & Run"):
+        if validate_no_negative_values(days_for_recovery_vulnerability):
+            st.success("Data saved successfully!")
+            flag_save = 1
+
+            buildings_df = water_permeation_and_recovery_days(Buildings, permeation_rate, loss_ratio_limit, days_for_recovery_vulnerability)
+        else:
+            st.warning("Please fix the negative values before saving.")
+
+
+with col4:
+    st.title("")
+    st.header("Output")
+    st.write("---")
+
+    # Convert back to dictionary on save
+    if flag_save == 1:
+        st.success("Restoration time for building typologies has been saved!")
+        st.dataframe(days_for_recovery_vulnerability, height=400, use_container_width=True)  # Display the updated DataFrame
+
+        # Display the Buildings DataFrame
+        st.write("---")
+        st.write("Dataset of Tomorrowville buildings with downtime information:")
+        st.dataframe(buildings_df, height=400, use_container_width=True)
+
+    st.write(
+        "Note: To complete the analysis for Goal 2, download the table and perform the calculations using Excel, Python, or MATLAB.")
+st.write("---")
+
+# Run the app with `streamlit run CEGE0037_Main_Streamlit.py`
+# streamlit run CEGE0037_Main_Streamlit.py
+
